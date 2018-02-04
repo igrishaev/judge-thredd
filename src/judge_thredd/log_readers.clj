9809db4104d7385c44d33a5473c40c91177681f8
Ivan Grishaev <ivan@grishaev.me>
2018-02-04 15:55:24 +0300

    initial commit

--- src/judge_thredd/log_readers.clj
+++ src/judge_thredd/log_readers.clj
@@ -40,10 +40,10 @@
      #object[org.joda.time.DateTime \"2015-08-07T08:45:00.000Z\"]"
   [datetime raw-time]
   (let [{:keys [hours minutes seconds]} (raw-time->datetime-map raw-time)]
-    (to-date (tc/plus datetime
-                      (tc/hours hours)
-                      (tc/minutes minutes)
-                      (tc/seconds seconds)))))
+    (tc/plus datetime
+             (tc/hours hours)
+             (tc/minutes minutes)
+             (tc/seconds seconds))))
 
 (defn drop-colon
   [speaker]
@@ -64,3 +64,82 @@
 (defn irc-messages
   [irc-log-resource]
   (->messages (IrcFreenodeLog.) irc-log-resource))
+
+;;
+;; The solution part.
+;;
+
+(def thread-window (* 60 5))
+
+(defn guess-reply
+  [message]
+  (let [parts (split message #": " 2)]
+    (when (> (count parts) 1)
+      (first parts))))
+
+(defn date-diff [d1 d2]
+  (tc/in-seconds (tc/interval d1 d2)))
+
+(defn make-threads [records-origin]
+
+  (loop [idx 0
+         messages []
+         users #{}
+         records records-origin
+         msg-by-user {}
+         rpl-by-user {}]
+
+    (if (empty? records)
+
+      (partition-by :thread
+                    (sort-by (juxt :thread :ts) messages))
+
+      (let [[ts user text] (first records)
+
+            users (conj users user)
+            reply (when-let [reply (guess-reply text)]
+                    (get users reply))
+
+            thread (when reply
+                     (some->> reply
+                              (get msg-by-user)
+                              (get messages)
+                              :thread))
+
+            thread (or thread
+                       (when-let [msg-idx (get msg-by-user user)]
+                         (when-let [last-msg (get messages msg-idx)]
+                           (let [diff (date-diff (:ts last-msg) ts)]
+                             (when (< diff thread-window)
+                               (when-let [last-reply (get rpl-by-user user)]
+                                 (when-let [msg-idx (get msg-by-user last-reply)]
+                                   (when-let [last-msg (get messages msg-idx)]
+                                     (:thread last-msg)))))))))
+
+            thread (or thread
+                       (when-let [last-msg (peek messages)]
+                         (when (= (last (:text last-msg)) \?)
+                           (:thread last-msg))))
+
+            thread (or thread idx)
+
+            message {:idx idx
+                     :ts ts
+                     :user user
+                     :reply reply
+                     :text text
+                     :thread thread}]
+
+        (recur (inc idx)
+               (conj messages message)
+               users
+               (rest records)
+               (assoc msg-by-user user idx)
+               (assoc rpl-by-user user reply))))))
+
+(defn msg->string
+  [{:keys [ts user text]}]
+  (format "%s %s> %s"
+          (tf/unparse hh-mm-ss->datetime ts)
+          user
+          text))
